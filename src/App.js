64836c8162fec240c0a11dc830f12760f70c7fe0
Anton Watson <anton.watson@cds.co.uk>
2022-12-12 18:53:26 +0000

    -WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
+const wid = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+
+]
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++ ) {
+      const columnOfThree = [i, i+wid, i + wid*2 ]
+      const decidedColor = currentColorArrangement[i]
+
+      if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+        columnOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++ ) {
+      const rowOfThree = [i, i + 1, i + 2]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+      if(notValid.includes(i)) continue
+
+      if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor)){
+        rowOfThree.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i < 39; i++ ) {
+      const columnOfFour = [i, i+wid, i + wid*2, i + wid*3 ]
+      const decidedColor = currentColorArrangement[i]
+
+      if ( columnOfFour.every(square => currentColorArrangement[square] === decidedColor)){
+        columnOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const checkForRowOfFour = () => {
+    for (let i = 0; i < 64; i++ ) {
+      const rowOfFour = [i, i + 1, i + 2, i + 3]
+      const decidedColor = currentColorArrangement[i]
+      const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+
+      if(notValid.includes(i)) continue
+
+      if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor)){
+        rowOfFour.forEach(square => currentColorArrangement[square] = '')
+      }
+    }
+  }
+
+  const moveIntoSquareBelow = () => {
+    for(let i = 0; i < 64 - wid; i++) {
+      const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+      const isFirstRow = firstRow.includes(i)
+
+      if(isFirstRow && currentColorArrangement[i] === '') {
+        let randomNumber = Math.floor(Math.random() * candyColors.length)
+        currentColorArrangement[i] = candyColors[randomNumber] 
+      }
+
+      if((currentColorArrangement[i + wid]) === '') {
+        currentColorArrangement[i + wid] = currentColorArrangement[i]
+        currentColorArrangement[i] = ''
+      }
+    }
+  }
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for(let i = 0; i < wid * wid; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)] 
+      randomColorArrangement.push(randomColor)
+    }
+    setCurrentColorArrangement(randomColorArrangement)
+    
+  }
+ 
+  useEffect(() => {
+    createBoard()
+  }, [])
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfFour()
+      checkForColumnOfThree()
+      checkForRowOfFour()
+      checkForRowOfThree()
+      moveIntoSquareBelow()
+      setCurrentColorArrangement([...currentColorArrangement])
+    }, 1000)
+    return () => clearInterval(timer)
+    
+
+  }, [checkForColumnOfFour, checkForColumnOfThree, checkForRowOfFour, checkForRowOfThree, moveIntoSquareBelow, currentColorArrangement])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((candyColor, index) => (
+          <img
+            key={index}
+            style={{backgroundColor: candyColor}}
+            alt={candyColor}
+          />
+
+        ))}
+      </div>
+    
     </div>
-  );
+  )
 }
 
 export default App;
